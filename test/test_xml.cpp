d8ad1715cdb3e6a99c0736f14e508625d2842c0a
Oleksii Toldaiev <alex.toldaiev@cern.ch>
2020-02-26 18:00:08 +0100

    Finally this works
    XML is a real pain though
    the only way to use it is to implement everything we need and never touch it again
    
    	modified:   test_xml.cpp

--- test/test_xml.cpp
+++ test/test_xml.cpp
@@ -9,6 +9,7 @@ and link with \c -lxml2
 */
 
 #include <stdio.h>
+#include <string.h>
 #include <iostream>
 #include <libxml2/libxml/xpath.h>
 
@@ -25,6 +26,20 @@ FILE * error_log;
 	}                                                                \
 }
 
+/* we don't need this because the XML is structured better now
+xmlNodePtr xmlNode_findChild_by_name(xmlNodePtr xml_node, const xmlChar* name)
+	{
+	// loop through children and look for the name
+	xmlNodePtr child=xml_node->children;
+	for (; child!=NULL; child=child->next)
+		{
+		//printf("%s\n", child->name);
+		//if (strcmp((const char*) child->name, (const char*) name)) return child;
+		if (xmlStrcmp(child->name, name)) return child;
+		}
+	return child;
+	}
+*/
 
 int main (int argc, char *argv[])
 {
@@ -43,7 +58,8 @@ if (argc < 1)
 const char* dsets_info_filename = argv[0];
 
 // xml document
-xmlDocPtr doc = xmlParseFile(dsets_info_filename);
+//xmlDocPtr doc = xmlParseFile(dsets_info_filename); // this does not allow options, which are needed to parse more than 256 elements....xml is really a pain
+xmlDocPtr doc = xmlReadFile(dsets_info_filename, NULL, XML_PARSE_HUGE);
 Stopif(!doc, return -1, "Error: unable to parse file %s\n", dsets_info_filename);
 
 // we'll search for tags with XPath
@@ -51,7 +67,39 @@ Stopif(!doc, return -1, "Error: unable to parse file %s\n", dsets_info_filename)
 xmlXPathContextPtr xpath_context = xmlXPathNewContext(doc);
 Stopif(!xpath_context, return -2, "Error: unable to create the XPath context\n");
 
+// get the nodes with a given tag name
+const xmlChar* xpath_hard_proc = (xmlChar*)"//hard_process";
+xmlXPathObjectPtr hard_procs = xmlXPathEvalExpression(xpath_hard_proc, xpath_context);
+Stopif(!hard_procs, return -3, "Error: could not XPath //hard_process");
+
+// loop over found tags
+for (int i=0; i<hard_procs->nodesetval->nodeNr; i++)
+	{
+	// dumping all content
+	//printf("%s\n", xmlNodeGetContent(hard_procs->nodesetval->nodeTab[i]));
+	// print their names
+	//printf("%s\n", hard_procs->nodesetval->nodeTab[i]->name);
+	xmlNodePtr hp = hard_procs->nodesetval->nodeTab[i];
+
+	// get xsec and dtags
+	if (xmlHasProp(hp, (const xmlChar*)"xsec"))
+		printf("xsec = %10.3g\t", atof((const char*)xmlGetProp(hp, (const xmlChar*)"xsec")));
+	else
+		printf("xsec = %10s", "NAN");
+
+	// the dsets are easy: the children are dsets + some random "text" files?? -- some internal XML crap
+	xmlNodePtr dset=hp->children;
+	for (; dset!=NULL; dset=dset->next)
+		{
+		if (xmlStrcmp(dset->name, (const xmlChar*)"dset") != 0) continue; // watch out how this comparison works!
+		printf(" %20s", (const char*)xmlGetProp(dset, (const xmlChar*)"dtag"));
+		}
+	printf("\n");
+	}
+
 // free all xml resources
+xmlXPathFreeObject(hard_procs);
+xmlXPathFreeContext(xpath_context);
 xmlFreeDoc(doc);
 return 0;
 }
